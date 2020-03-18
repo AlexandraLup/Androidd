9553837385636fe46bf7196394b0def5d8326832
Alexandra <alexandra,lupuleasa@info.uaic.ro>
2020-03-18 18:50:41 +0200

    lab5ex2
    Pentru a functiona corespunzator trebuie sa fie setata o valoare in listView

--- lab2/app/src/main/java/com/example/lab2/Optiuni.java
+++ lab2/app/src/main/java/com/example/lab2/Optiuni.java
@@ -17,6 +17,7 @@ public class Optiuni extends AppCompatActivity {
         setContentView(R.layout.activity_optiuni);
 
         ImageView image = (ImageView) findViewById(R.id.call);
+        ImageView fileUpload = (ImageView) findViewById(R.id.fileUpload);
 
         image.setOnClickListener(new OnClickListener() {
             public void onClick(View v)
@@ -24,6 +25,15 @@ public class Optiuni extends AppCompatActivity {
                 Intent intent1 = new Intent(Intent.ACTION_SEND, Uri.fromParts("sms", "0741621236", null));
                 intent1.setType("text/plain");
                 startActivity(intent1);
+            }
+
+        });
+
+        fileUpload.setOnClickListener(new OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent intent2 = new Intent(Optiuni.this, FileUploadActivity.class);
+                startActivity(intent2);
             }
 
         });
